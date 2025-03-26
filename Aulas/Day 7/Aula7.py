a11c5f7c64cf1e739e320bc6c30e146326804dcf
Betoesu <pedrosarmento1412@gmail.com>
2025-03-26 15:08:56 -0300

    mudança

--- Aulas/Day 7/Aula7.py
+++ Aulas/Day 7/Aula7.py
@@ -1,16 +1,28 @@
 import random
+
 word = ["alfa" , "beta", "charlie"]
+
 chosen_word = random.choice(word)
 print(chosen_word)
-guess = str.lower(input("Guess a letter: "))
 blank = []
 chosen_word_list = list(chosen_word)
-print(chosen_word_list)
+x = 1
+
+guess = str.lower(input("Guess a letter: "))
 for letter in range (0,len(chosen_word)):
     blank.append ("_")
     if guess == chosen_word[letter]:
-        del chosen_word_list[letter]
-        chosen_word_list.append(guess)
+        del blank[letter]
+        blank.append(guess)
+contador = 0
+
+        
+
+
+
+
+
+
 
 blank = "".join(blank)
 print(blank)
