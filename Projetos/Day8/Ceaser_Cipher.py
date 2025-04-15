584d85a8ae876819469d5fae07428b7251f62ca3
Betoesu <pedrosarmento1412@gmail.com>
2025-04-15 20:21:11 -0300

    rhyan

--- Projetos/Day8/Ceaser_Cipher.py
+++ Projetos/Day8/Ceaser_Cipher.py
@@ -1,5 +1,13 @@
-
-
+def encrypt(message): 
+    crypt_message = ""
+    for letter in message:
+        if letter == " ":
+            crypt_message += " "
+        else:  
+            position = alphabet.index(letter)
+            letter = alphabet[position + number]
+            crypt_message += letter
+    print(crypt_message)
 
 
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
@@ -10,17 +18,18 @@ alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n"
 decode_or_encode = input("Type 'encode' to encrypt, type 'decode' to decrypt: ").lower()
 if decode_or_encode == "encode":
     message = input("Type your message: ")
-    number = input("Type your shift number: ")
+    number = int(input("Type your shift number: "))
      #
 
 elif decode_or_encode == "decode":
     message = input("Type your message: ")
-    number = input("Type your shift number: ")
-            #
+    number = int(input("Type your shift number: "))
+    #
 
 else:
     print("")
     input('''You type the worng command.      
 Type Enter to go again: ''')
     print("")
-        
+    
+encrypt(message)
