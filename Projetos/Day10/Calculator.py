34d0ee378eafab05b9d5b9b17ff96b7bd9cbfd6c
Betoesu <pedrosarmento1412@gmail.com>
2025-04-28 16:00:51 -0300

    28/04

---
+++ Projetos/Day10/Calculator.py
@@ -0,0 +1,62 @@
+def solicitar_operador():
+    print('''Escolha a Operação
++
+-
+*     
+ /''')
+    while True:
+        simbolo = input("Escolha um símbolo: ").strip()
+        if simbolo in ["+","-","*","/"]:
+            return simbolo
+        else:
+            print("Você digitou o símbolo errado")
+
+
+
+n1 = int(input("Digite seu primeiro número: "))
+n2 = int(input("Digite seu segundo número: "))
+
+simbolo = solicitar_operador()
+
+if solicitar_operador() == "+":
+    resultado = n1 + n2
+
+elif simbolo == "-":
+    resultado = n1 - n2
+
+elif simbolo == "*":
+    resultado = n1 * n2
+
+elif simbolo == "/":
+    resultado = n1 / n2
+
+
+print(resultado)
+
+continuar = input("Quer continuar calculando com esse resultado? ").lower()
+
+while continuar in ["sim", "ss", "s", "yes"]:
+    n1 = resultado
+    n2 = int(input("Digite seu número: "))
+    simbolo = solicitar_operador()
+
+
+    if simbolo == "+":
+        resultado = n1 + n2
+    elif simbolo == "-":
+        resultado = n1 - n2
+    elif simbolo == "*":
+        resultado = n1 * n2
+    elif simbolo == "/":
+        resultado = n1 / n2
+
+    print(resultado)
+    continuar = input("Quer continuar calculando com esse resultado? ").lower()
+
+print(f"Esse é seu resultado final: {resultado}")
+
+
+    
+
+
+        
