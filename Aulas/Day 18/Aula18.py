37a3798c4a5902dd5ee2c41713ea252cb20ce64c
Betoesu <pedrosarmento1412@gmail.com>
2025-05-19 18:15:46 -0300

    Dia 18

--- Aulas/Day 18/Aula18.py
+++ Aulas/Day 18/Aula18.py
@@ -5,18 +5,18 @@ import random
 turtle = Turtle()
 color = colormode(255)
 
-# turtle.shape("turtle")
-# turtle.color("red")
+turtle.shape("turtle")
+turtle.color("red")
 
-# for _ in range(4):
-#     turtle.forward(100)
-#     turtle.right(90)
+for _ in range(4):
+    turtle.forward(100)
+    turtle.right(90)
 
-# for _ in range(15):
-#     turtle.forward(10)
-#     turtle.penup()
-#     turtle.forward(10)
-#     turtle.pendown()
+for _ in range(15):
+    turtle.forward(10)
+    turtle.penup()
+    turtle.forward(10)
+    turtle.pendown()
 
 
 
--- Projetos/Day18/Geometrical_pictures.py
+++ Projetos/Day18/Geometrical_pictures.py
@@ -16,36 +16,3 @@ for _ in range(8):
 
 
 
-def random_walk():
-    number = random.randint(1,2)
-    
-
-    if number == 1:
-        turtle.right(random.choice(directions))
-        
-
-    else:
-        turtle.left(random.choice(directions))
-    
-    turtle.forward(30)
-
-
-def random_color():
-    r = random.randint(0,255)
-    g = random.randint(0,255)
-    b = random.randint(0,255)
-    color = (r,g,b)
-    return color
-
-directions = [0,90,180,270]
-
-turtle.pensize(150)
-turtle.speed("fastest")
-
-for _ in range (200):
-    color = random_color()
-    turtle.color(color)
-    random_walk()
-
-    screen = Screen()
-    screen.exitonclick()
