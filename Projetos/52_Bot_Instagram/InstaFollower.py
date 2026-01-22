6b9aa7a7785c84c9fcb32186eae40114f2208123
Betoesu <pedrosarmento1412@gmail.com>
2026-01-22 14:53:25 -0300

    Testar Código

--- Projetos/52_Bot_Instagram/InstaFollower.py
+++ Projetos/52_Bot_Instagram/InstaFollower.py
@@ -6,6 +6,14 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
+#Importando erros para usar try e except
+from selenium.common.exceptions import (
+    NoSuchElementException,
+    TimeoutException,
+    StaleElementReferenceException,
+    ElementClickInterceptedException
+)
+
 
 class InstaFollower:
     def __init__(self):
@@ -15,29 +23,63 @@ class InstaFollower:
         self.driver = webdriver.Chrome(options=chrome_options)
     
 
+
+
+
+
     def login(self):
         url = "https://www.instagram.com/accounts/login/"
         self.driver.get(url)
         
-        wait = WebDriverWait(self.driver, 20)
+        self.wait = WebDriverWait(self.driver, 20)
 
+        try:
         #Espera o elemento estar na página para executar
-        username = wait.until(EC.element_to_be_clickable((By.NAME,"username")))
-        username.send_keys(USERNAME)
+            username = self.wait.until(EC.element_to_be_clickable((By.NAME,"username")))
+            username.send_keys(USERNAME)
 
-        password = self.driver.find_element(By.NAME, value="password")
-        password.send_keys(PASSWORD)
+            password = self.driver.find_element(By.NAME, value="password")
+            password.send_keys(PASSWORD)
 
-        submit_button = self.driver.find_element(By.XPATH, value='//*[@id="loginForm"]/div[1]/div[3]/button')
-        submit_button.click()
+            submit_button = self.driver.find_element(By.XPATH, value='//*[@id="loginForm"]/div[1]/div[3]/button')
+            submit_button.click()
 
-        
-        #Espera aparecer om pop up de salvar informação
-        salvar_info_button = wait.until(EC.element_to_be_clickable((By.XPATH,"//div[contains(text(), 'Agora não')]")))
-        salvar_info_button.click()
+            #Espera aparecer om pop up de salvar informação
+            salvar_info_button = self.wait.until(EC.element_to_be_clickable((By.XPATH,"//div[contains(text(), 'Agora não')]")))
+            salvar_info_button.click()
+
+            
+
+        except (TimeoutException,NoSuchElementException):
+            username = self.wait.until(EC.element_to_be_clickable((By.NAME,"email")))
+            username.send_keys(USERNAME)
+
+            password = self.driver.find_element(By.NAME, value="pass")
+            password.send_keys(PASSWORD)
+
+            submit_button = self.driver.find_element(By.XPATH, value='//*[@id="login_form"]/div/div[1]/div/div[3]/div/div/div/div[1]/div/span/span')
+            submit_button.click()
+
+            time.sleep(2)
+
+            continuar_button = self.wait.until(EC.element_to_be_clickable((By.XPATH,f"//span[contains(text(), {USERNAME})]")))
+            continuar_button.click()
+
+            #Espera aparecer om pop up de salvar informação
+            salvar_info_button = self.wait.until(EC.element_to_be_clickable((By.XPATH,"//div[contains(text(), 'Agora não')]")))
+            salvar_info_button.click()
+
+
+
+
 
         #Tempo para carregar sem erro
-        time.sleep(5)
+        time.sleep(3)
+
+
+
+
+
 
 
 
@@ -54,17 +96,42 @@ class InstaFollower:
         pesquisa_input = self.driver.find_element(By.XPATH, '//*[@aria-label="Entrada da pesquisa"]')
 
         pesquisa_input.send_keys(SIMILAR_ACCOUNT)
+
+        
         
         #Clica no perfil escolhido
-        conta_escolhida = self.driver.find_element(By.XPATH,'//*[@href="/vinlandsaga_official/"]')
+        conta_escolhida = self.wait.until(EC.element_to_be_clickable((By.XPATH,'//*[@href="/vinlandsaga_official/"]')))
         conta_escolhida.click()
 
         time.sleep(2)
 
         #Clica na aba de seguidores
-        seguidores = self.driver.find_element(By.XPATH, '//a[@href="/vinlandsaga_official/followers/"]')
+        seguidores = self.wait.until(EC.element_to_be_clickable((By.XPATH, '//a[@href="/vinlandsaga_official/followers/"]')))
         seguidores.click()
 
-        #Falta clicar automaticamente para seguir e descobrir como scrollar para baixo
+       
+
+        time.sleep(5)
+        modal_xpath = "/html/body/div[5]/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]"
+        modal = self.wait.until(EC.presence_of_element_located((By.XPATH, modal_xpath)))
+        for n_account in range(2,10):
+                xpath = f'//div[@role="dialog"]/div/div[2]/div/div/div[3]/div[1]/div/div[{n_account}]/div/div/div/div[3]/div/button'
+                follow_button = self.wait.until(EC.element_to_be_clickable((By.XPATH, xpath)))
+                follow_button.click()
+                time.sleep(2)
+
+                botão_seguido_ou_solicitado = self.driver.find_element(By.XPATH,f'//div[@role="dialog"]/div/div[2]/div/div/div[3]/div[1]/div/div[{n_account}]/div/div/div/div[3]/div/button/div/div')
+                seguido_ou_solicitado = botão_seguido_ou_solicitado.text
+
+
+                if seguido_ou_solicitado == "Seguindo" or seguido_ou_solicitado == "Solicitado":
+                    #Maneira com JavaScript de scrollar a tela
+                    self.driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", modal)
+
+
+
+
+
+
     def follow(self):
         pass
